0c1402e4605d46c28200ab107c88c5c75284948c
michaelvong <mvong006@ucr.edu>
2024-04-09 23:30:36 -0700

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { Client, IntentsBitField, EmbedBuilder } = require('discord.js');
 const { CommandHandler } = require('djs-commander');
 const path = require('path');
 const { Player } = require('discord-player');
+const mongoose = require('mongoose');
 
 const client = new Client({
     intents : [
@@ -20,44 +21,28 @@ player.extractors.loadDefault(); //important line for player to work
 new CommandHandler({
     client, // this simplifies writing "client : client" since they have same name
     commandsPath : path.join(__dirname, '..', 'commands'),
+    eventsPath : path.join(__dirname, '..', 'events'),
     testServer : '1226291241906343936'
 })
 const exampleEmbed = new EmbedBuilder().setTitle('ExampleEmbed')
 .setDescription('Example');
 
-client.on('ready', (c) => {
-    console.log(`${c.user.tag} is ready`);
-})
 
-client.on('messageCreate', (message) => {
-    //console.log(message.content);
-});
-
-client.on('interactionCreate', (interaction) => {
-    if(!interaction.isChatInputCommand()) { return; }
-
-
-    if(interaction.commandName === 'test') {
-        interaction.reply('the test command is working!');
-    }
-    if(interaction.commandName === 'ping') {
-        interaction.reply('pong!');
-    }
-    /*if(interaction.commandName === 'quiz') {
-        const link = interaction.options.get('playlist-link').value; //stores user input into link
-        //interaction.reply(link.value);
-        
-        const startEmbed = new EmbedBuilder().setTitle('Starting game!')
-        interaction.reply({embeds: [startEmbed]});
-    }*/
-});
 
   //https://open.spotify.com/playlist/04ETACGQVjIH92ITiwC596?si=6ac7a49f42d54e81
 
-client.login(
-    process.env.TOKEN
-);
 
+(async () => {
+    try {
+        await mongoose.connect(process.env.mongoURL);
+        console.log('conncted to db');
+        client.login(
+            process.env.TOKEN
+        );
+    } catch (error) {
+        console.log(error);
+    }
+})();
 
 
 player.events.on('playerError', (event) => {
