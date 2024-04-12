00ee7159d1d53ec9355fc8dd7b76e8f1312661af
michaelvong <mvong006@ucr.edu>
2024-04-11 23:23:20 -0700

    added guild parameter

--- utils/getTopPlayer.js
+++ utils/getTopPlayer.js
@@ -1,8 +1,11 @@
 const { Client , Message } = require('discord.js');
 const Score = require('../models/Score');
 
-const getTopPlayer = async () => {
-    const top_player = await Score.findOne().sort({score:-1});
+const getTopPlayer = async (guildId) => {
+    const query = {
+        guildId : guildId.toString(),
+    }
+    const top_player = await Score.findOne(query).sort({score:-1}); //sorts the orders in descending order
     return top_player;
 }
 
